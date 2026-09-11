3b8980fa7f6617d3accbf0f3514150a7ba020abe
puttu03 <saisanjeev2004@gmail.com>
2026-09-11 02:14:26 +0000

    Updated code

--- backend/services/flag_service.py
+++ backend/services/flag_service.py
@@ -1,13 +1,12 @@
-from datatime import datatime, timedelta
-from models import Report 
+from datetime import datetime, timedelta
+from models import Report
 from config import FLAG_HISTORY_DAYS, FLAG_DISTANCE_METERS
 from services.geo import distance_meters
 
-def calculate_flag(issue_name, latitude, logitude):
-    since = datetime.utcnow() - timedelta(FLAG_HISTORY_DAYS)
-    report = Report.query.filter(Report.issue_name == issue_name, Report.created_at >= since).all()
+def calculate_flag(issue_name, latitude, longitude):
+    since = datetime.utcnow() - timedelta(days=FLAG_HISTORY_DAYS)
+    reports = Report.query.filter(Report.issue_name == issue_name, Report.created_at >= since).all()
     for report in reports:
-        if distance_meters(latitude,logitude, report.latitude, report.logitude) <= FLAG_DISTANCE_METERS:
+        if distance_meters(latitude, longitude, report.latitude, report.longitude) <= FLAG_DISTANCE_METERS:
             return 'YES'
-
     return 'NO'
